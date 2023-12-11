425dc1d5354d2c59f08ce8b033817a547e9c0ff0
Blade <150795617+Bladehere@users.noreply.github.com>
2023-12-11 21:32:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['919946837537', 'B L A D E', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
